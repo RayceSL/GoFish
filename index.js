06a6c0108c6015bd9b775581318e37bf455bc803
RayceSL <raycel@spitwspots.com>
2024-07-09 10:08:13 -0800

    Players can now search thru e/o hands for matches

--- index.js
+++ index.js
@@ -1,69 +1,63 @@
-/*
-♥ = 0
-♣ = 1
-♦ = 2
-♠ = 3
-*/
-
+// ♥ ♣ ♦ ♠
 let stock = [
     // {"name": "Red Joker", "rank": "13", "suit": "4"},
     // {"name": "Black Joker", "rank": "13", "suit": "4"},
 
-    {"name": "A♥", "rank": "0", "suit": "0"},
-    {"name": "2♥", "rank": "1", "suit": "0"},
-    {"name": "3♥", "rank": "2", "suit": "0"},
-    {"name": "4♥", "rank": "3", "suit": "0"},
-    {"name": "5♥", "rank": "4", "suit": "0"},
-    {"name": "6♥", "rank": "5", "suit": "0"},
-    {"name": "7♥", "rank": "6", "suit": "0"},
-    {"name": "8♥", "rank": "7", "suit": "0"},
-    {"name": "9♥", "rank": "8", "suit": "0"},
-    {"name": "10♥", "rank": "9", "suit": "0"},
-    {"name": "J♥", "rank": "10", "suit": "0"},
-    {"name": "Q♥", "rank": "11", "suit": "0"},
-    {"name": "K♥", "rank": "12", "suit": "0"},
+    {"name": "A♥",  "value": 14, "suit": "Hearts (♥)", "rank": "Ace",   "rankPlural": "Aces"},
+    {"name": "2♥",  "value": 2,  "suit": "Hearts (♥)", "rank": "Two",   "rankPlural": "Twos"},
+    {"name": "3♥",  "value": 3,  "suit": "Hearts (♥)", "rank": "Three", "rankPlural": "Threes"},
+    {"name": "4♥",  "value": 4,  "suit": "Hearts (♥)", "rank": "Four",  "rankPlural": "Fours"},
+    {"name": "5♥",  "value": 5,  "suit": "Hearts (♥)", "rank": "Five",  "rankPlural": "Fives"},
+    {"name": "6♥",  "value": 6,  "suit": "Hearts (♥)", "rank": "Six",   "rankPlural": "Sixes"},
+    {"name": "7♥",  "value": 8,  "suit": "Hearts (♥)", "rank": "Seven", "rankPlural": "Sevens"},
+    {"name": "8♥",  "value": 7,  "suit": "Hearts (♥)", "rank": "Eight", "rankPlural": "Eights"},
+    {"name": "9♥",  "value": 9,  "suit": "Hearts (♥)", "rank": "Nine",  "rankPlural": "Nines"},
+    {"name": "10♥", "value": 10, "suit": "Hearts (♥)", "rank": "Ten",   "rankPlural": "Tens"},
+    {"name": "J♥",  "value": 11, "suit": "Hearts (♥)", "rank": "Jack",  "rankPlural": "Jacks"},
+    {"name": "Q♥",  "value": 12, "suit": "Hearts (♥)", "rank": "Queen", "rankPlural": "Queens"},
+    {"name": "K♥",  "value": 13, "suit": "Hearts (♥)", "rank": "King",  "rankPlural": "Kings"},
 
-    {"name": "A♣", "rank": "0", "suit": "1"},
-    {"name": "2♣", "rank": "1", "suit": "1"},
-    {"name": "3♣", "rank": "2", "suit": "1"},
-    {"name": "4♣", "rank": "3", "suit": "1"},
-    {"name": "5♣", "rank": "4", "suit": "1"},
-    {"name": "6♣", "rank": "5", "suit": "1"},
-    {"name": "7♣", "rank": "6", "suit": "1"},
-    {"name": "8♣", "rank": "7", "suit": "1"},
-    {"name": "9♣", "rank": "8", "suit": "1"},
-    {"name": "10♣", "rank": "9", "suit": "1"},
-    {"name": "J♣", "rank": "10", "suit": "1"},
-    {"name": "Q♣", "rank": "11", "suit": "1"},
-    {"name": "K♣", "rank": "12", "suit": "1"},
+    {"name": "A♣",  "value": 14, "suit": "Clubs (♣)", "rank": "Ace",   "rankPlural": "Aces"},
+    {"name": "2♣",  "value": 2,  "suit": "Clubs (♣)", "rank": "Two",   "rankPlural": "Twos"},
+    {"name": "3♣",  "value": 3,  "suit": "Clubs (♣)", "rank": "Three", "rankPlural": "Threes"},
+    {"name": "4♣",  "value": 4,  "suit": "Clubs (♣)", "rank": "Four",  "rankPlural": "Fours"},
+    {"name": "5♣",  "value": 5,  "suit": "Clubs (♣)", "rank": "Five",  "rankPlural": "Fives"},
+    {"name": "6♣",  "value": 6,  "suit": "Clubs (♣)", "rank": "Six",   "rankPlural": "Sixes"},
+    {"name": "7♣",  "value": 8,  "suit": "Clubs (♣)", "rank": "Seven", "rankPlural": "Sevens"},
+    {"name": "8♣",  "value": 7,  "suit": "Clubs (♣)", "rank": "Eight", "rankPlural": "Eights"},
+    {"name": "9♣",  "value": 9,  "suit": "Clubs (♣)", "rank": "Nine",  "rankPlural": "Nines"},
+    {"name": "10♣", "value": 10, "suit": "Clubs (♣)", "rank": "Ten",   "rankPlural": "Tens"},
+    {"name": "J♣",  "value": 11, "suit": "Clubs (♣)", "rank": "Jack",  "rankPlural": "Jacks"},
+    {"name": "Q♣",  "value": 12, "suit": "Clubs (♣)", "rank": "Queen", "rankPlural": "Queens"},
+    {"name": "K♣",  "value": 13, "suit": "Clubs (♣)", "rank": "King",  "rankPlural": "Kings"},
 
-    {"name": "K♦", "rank": "12", "suit": "2"},
-    {"name": "Q♦", "rank": "11", "suit": "2"},
-    {"name": "J♦", "rank": "10", "suit": "2"},
-    {"name": "10♦", "rank": "9", "suit": "2"},
-    {"name": "9♦", "rank": "8", "suit": "2"},
-    {"name": "8♦", "rank": "7", "suit": "2"},
-    {"name": "7♦", "rank": "6", "suit": "2"},
-    {"name": "6♦", "rank": "5", "suit": "2"},
-    {"name": "5♦", "rank": "4", "suit": "2"},
-    {"name": "4♦", "rank": "3", "suit": "2"},
-    {"name": "3♦", "rank": "2", "suit": "2"},
-    {"name": "2♦", "rank": "1", "suit": "2"},
-    {"name": "A♦", "rank": "0", "suit": "2"},
+    {"name": "A♦",  "value": 14, "suit": "Diamonds (♦)", "rank": "Ace",   "rankPlural": "Aces"},
+    {"name": "2♦",  "value": 2,  "suit": "Diamonds (♦)", "rank": "Two",   "rankPlural": "Twos"},
+    {"name": "3♦",  "value": 3,  "suit": "Diamonds (♦)", "rank": "Three", "rankPlural": "Threes"},
+    {"name": "4♦",  "value": 4,  "suit": "Diamonds (♦)", "rank": "Four",  "rankPlural": "Fours"},
+    {"name": "5♦",  "value": 5,  "suit": "Diamonds (♦)", "rank": "Five",  "rankPlural": "Fives"},
+    {"name": "6♦",  "value": 6,  "suit": "Diamonds (♦)", "rank": "Six",   "rankPlural": "Sixes"},
+    {"name": "7♦",  "value": 8,  "suit": "Diamonds (♦)", "rank": "Seven", "rankPlural": "Sevens"},
+    {"name": "8♦",  "value": 7,  "suit": "Diamonds (♦)", "rank": "Eight", "rankPlural": "Eights"},
+    {"name": "9♦",  "value": 9,  "suit": "Diamonds (♦)", "rank": "Nine",  "rankPlural": "Nines"},
+    {"name": "10♦", "value": 10, "suit": "Diamonds (♦)", "rank": "Ten",   "rankPlural": "Tens"},
+    {"name": "J♦",  "value": 11, "suit": "Diamonds (♦)", "rank": "Jack",  "rankPlural": "Jacks"},
+    {"name": "Q♦",  "value": 12, "suit": "Diamonds (♦)", "rank": "Queen", "rankPlural": "Queens"},
+    {"name": "K♦",  "value": 13, "suit": "Diamonds (♦)", "rank": "King",  "rankPlural": "Kings"},
 
-    {"name": "K♠", "rank": "12", "suit": "3"},
-    {"name": "Q♠", "rank": "11", "suit": "3"},
-    {"name": "J♠", "rank": "10", "suit": "3"},
-    {"name": "10♠", "rank": "9", "suit": "3"},
-    {"name": "9♠", "rank": "8", "suit": "3"},
-    {"name": "8♠", "rank": "7", "suit": "3"},
-    {"name": "7♠", "rank": "6", "suit": "3"},
-    {"name": "6♠", "rank": "5", "suit": "3"},
-    {"name": "5♠", "rank": "4", "suit": "3"},
-    {"name": "4♠", "rank": "3", "suit": "3"},
-    {"name": "3♠", "rank": "2", "suit": "3"},
-    {"name": "2♠", "rank": "1", "suit": "3"},
-    {"name": "A♠", "rank": "0", "suit": "3"}
+    {"name": "A♠",  "value": 14, "suit": "Spades (♠)", "rank": "Ace",   "rankPlural": "Aces"},
+    {"name": "2♠",  "value": 2,  "suit": "Spades (♠)", "rank": "Two",   "rankPlural": "Twos"},
+    {"name": "3♠",  "value": 3,  "suit": "Spades (♠)", "rank": "Three", "rankPlural": "Threes"},
+    {"name": "4♠",  "value": 4,  "suit": "Spades (♠)", "rank": "Four",  "rankPlural": "Fours"},
+    {"name": "5♠",  "value": 5,  "suit": "Spades (♠)", "rank": "Five",  "rankPlural": "Fives"},
+    {"name": "6♠",  "value": 6,  "suit": "Spades (♠)", "rank": "Six",   "rankPlural": "Sixes"},
+    {"name": "7♠",  "value": 8,  "suit": "Spades (♠)", "rank": "Seven", "rankPlural": "Sevens"},
+    {"name": "8♠",  "value": 7,  "suit": "Spades (♠)", "rank": "Eight", "rankPlural": "Eights"},
+    {"name": "9♠",  "value": 9,  "suit": "Spades (♠)", "rank": "Nine",  "rankPlural": "Nines"},
+    {"name": "10♠", "value": 10, "suit": "Spades (♠)", "rank": "Ten",   "rankPlural": "Tens"},
+    {"name": "J♠",  "value": 11, "suit": "Spades (♠)", "rank": "Jack",  "rankPlural": "Jacks"},
+    {"name": "Q♠",  "value": 12, "suit": "Spades (♠)", "rank": "Queen", "rankPlural": "Queens"},
+    {"name": "K♠",  "value": 13, "suit": "Spades (♠)", "rank": "King",  "rankPlural": "Kings"}
 ];
 
 let player0 = [];
@@ -144,13 +138,67 @@ function findBooks(player, books) {
     }
 }
 
+function findMatches(fromPlayer, fromIndex, toPlayer) {
+    for (i = 0; i < fromPlayer.length; i++) {
+        if (toPlayer[fromIndex].rank == fromPlayer[0].rank) {
+            // deal to THE END of toPlayer!!
+            // Push the matching card, which is at index 0, from the fromPlayer to the toPlayer
+            // That way, the card we're comparing from doesn't change its index
+            toPlayer.push(fromPlayer[0]);
+            fromPlayer.shift(); // removes the card from fromPlayer's hand
+        } else {
+            fromPlayer.push(fromPlayer[0]); // If the matching card isn't found at index 0,
+            fromPlayer.shift();             // Move it to the back and keep checking
+        }
+    } 
+}
+
 // For now, I'll proceed with 3 players only, and player0 is always the dealer
+// This deals 7 cards to each player
 for (i = 0; i < 7; i++) {
     dealRand(stock, player0);
     dealRand(stock, player1);
     dealRand(stock, player2);
 }
 
-console.log(player0);
-console.log(player1);
-console.log(player2);
+// First turn should be player1 "Abby", then player2 "Bob", then player0 "Y/N"
+/*
+Stupid bot turn structure:
+
+1. Choose a random rank from own hand and a random player to take from
+
+2. Check the chosen player's hand for cards of the matching rank
+
+3. Take all cards of matching rank from them, pass turn to next player
+
+4. If they have no cards of matching rank, take one card from the stock
+
+5. Pass turn to next player
+*/
+
+console.log(`\n${playerNames[0]}'s hand: ${player0.length} cards.`);
+console.log(`${playerNames[1]}'s hand: ${player1.length} cards.`);
+console.log(`${playerNames[2]}'s hand: ${player2.length} cards.`);
+
+// Choose random rank
+let randRank = (Math.floor(Math.random() * player1.length));
+
+// Choose random player
+let otherPlayers = [
+    {"name": "Y/N", "handName": player0}, 
+    {"name": "Bob", "handName": player2}
+];
+let randPlayer = (Math.floor(Math.random() * otherPlayers.length));
+let randHandName = otherPlayers[randPlayer].handName;
+
+// Ask for cards
+console.log(`\nAbby asks ${otherPlayers[randPlayer].name} for all their ${player1[randRank].rankPlural}.`);
+
+// Check for matching ranks
+// Find ALL matching cards... there could only ever be three others found
+findMatches(randHandName, randRank, player1);
+console.log(`${otherPlayers[randPlayer].name} searches their hand, and may or may not have given Abby any cards.`);
+
+console.log(`\n${playerNames[0]}'s hand: ${player0.length} cards.`);
+console.log(`${playerNames[1]}'s hand: ${player1.length} cards.`);
+console.log(`${playerNames[2]}'s hand: ${player2.length} cards.`);
